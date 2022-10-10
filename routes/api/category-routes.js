d3d2c204a9cd7d6308d8ea5f833fadef2652efb2
Christian Smith <christian.46.smith@gmail.com>
2022-10-10 16:13:51 -0400

    Add update and delete categories in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,16 +33,51 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryAdd = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryAdd);
+  } catch (err) {
+    res.status(500).json('{"message": "Error adding category"}');
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryUpdate = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(500).json('{"message": "Error updating category"}');
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryUpdate = await Category.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(500).json('{"message": "Error deleting category"}');
+  }
 });
 
 module.exports = router;
